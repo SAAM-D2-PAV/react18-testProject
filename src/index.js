f69cedd9b19bae39fdbd2d90fc50610e0f5b5377
Mathieu Le Ray <leraymathieu@gmail.com>
2023-10-24 14:45:37 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -5,13 +5,33 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const composant = () => {
+  return <h1 className="final">Hello World 2!</h1>
+}
+
+const h1 = <h1 className='salut'> Hello World!!!</h1>
+
+const reactElement = React.createElement('h1', {className:'my-class'}, 'Hello World!')
+console.log(reactElement);
+// webpack transforme en objet javascript 
+// const reactElement = {
+//   type: 'h1',
+//   props: {
+//     className: 'my-class',
+//     children: 'Hello World!'
+//   }
+// }
+
+const tree = React.createElement('div', {className:'my-div'}, reactElement)
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  tree
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
